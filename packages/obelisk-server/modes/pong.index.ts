5b2d3c4ada7a6d7c0fd8ccea0b7b928ea31220f0
Sacha Sayan <sacha.sayan@rangle.io>
2020-04-09 14:13:03 -0400

    Update pong.index.ts

--- packages/obelisk-server/modes/pong.index.ts
+++ packages/obelisk-server/modes/pong.index.ts
@@ -90,7 +90,7 @@ function inputLoop(t: number){
   let motorImprecision = (5 * Math.sin(1 * Math.PI * (t/1000)));
   gameState.paddles[1] = Math.round(
     ((1-certainty) * (matrix.height()/2)) + //Start with the middle
-    (certainty * (gameState.ball.y + motorImprecision)) // Where we're aiming for, with motor imprecision
+    (certainty * (gameState.ball.y)) // Where we're aiming for, with motor imprecision
   );
 }
 
